a2593d079b18410937f1ad7560524a09e1a72b58
Eudalet <47557352+Hevne@users.noreply.github.com>
2020-01-04 16:56:28 +0100

    solved random angles

--- Project/eAngles2rotM.m
+++ Project/eAngles2rotM.m
@@ -8,17 +8,17 @@ function [R] = eAngles2rotM(yaw, pitch, roll)
 % Outputs:
 %	R: rotation matrix
 
-r_yaw = [cos(yaw), sin(yaw), 0;
-        -sin(yaw), cos(yaw), 0;
+r_yaw = [cosd(yaw), sind(yaw), 0;
+        -sind(yaw), cosd(yaw), 0;
         0, 0, 1];
     
-r_pitch = [cos(pitch), 0, -sin(pitch);
+r_pitch = [cosd(pitch), 0, -sind(pitch);
         0, 1, 0;
-        sin(pitch), 0, cos(pitch)];
+        sind(pitch), 0, cosd(pitch)];
     
 r_roll = [1, 0, 0;
-        0, cos(roll), sin(roll);
-        0, -sin(roll), cos(roll)];
+        0, cosd(roll), sind(roll);
+        0, -sind(roll), cosd(roll)];
 
 R = transpose(r_roll * r_pitch *r_yaw);
 
